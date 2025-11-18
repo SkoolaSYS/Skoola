6817cfd442d536fa0cb78d71d401c4dfcf4beeac
hanacesa <hanisa.hanim53@gmail.com>
2025-11-18 22:47:38 +0800

    layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -30,7 +30,7 @@
     <link href="{{ url('assets/plugins/global/plugins.bundle.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ url('assets/css/style.bundle.css') }}" rel="stylesheet" type="text/css" />
 
-    <link href="(assets/plugins/global/plugins.bundle.css" rel="stylesheet" type="text/css" />
+    
     <script src="assets/plugins/global/plugins.bundle.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/apexcharts@latest"></script>
 
@@ -300,50 +300,53 @@
             </div>
 
             <!--begin::Javascript-->
-            <script>
-                var hostUrl = "assets/";
-            </script>
-            <!--begin::Global Javascript Bundle(mandatory for all pages)-->
-            <script src="assets/plugins/global/plugins.bundle.js"></script>
-            <script src="assets/js/scripts.bundle.js"></script>
-            <!--end::Global Javascript Bundle-->
-            <!--begin::Vendors Javascript(used for this page only)-->
-            <script src="assets/plugins/custom/fullcalendar/fullcalendar.bundle.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/map.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/geodata/continentsLow.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/geodata/usaLow.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZonesLow.js"></script>
-            <script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZoneAreasLow.js"></script>
-            <script src="assets/plugins/custom/datatables/datatables.bundle.js"></script>
-            <!--end::Vendors Javascript-->
-            <!--begin::Custom Javascript(used for this page only)-->
-            <script src="{{ asset('assets/js/widgets.bundle.js') }}"></script>
-            <script src="assets/js/custom/widgets.js"></script>
-            <script src="assets/js/custom/apps/chat/chat.js"></script>
-            <script src="assets/js/custom/utilities/modals/upgrade-plan.js"></script>
-            <script src="assets/js/custom/utilities/modals/create-campaign.js"></script>
-            <script src="assets/js/custom/utilities/modals/users-search.js"></script>
-            <script src="assets/js/custom/apps/customers/list/export.js"></script>
-            <script src="assets/js/custom/apps/customers/list/list.js"></script>
-            <script src="assets/plugins/custom/datatables/datatables.bundle.js"></script>
+<script>
+    var hostUrl = "{{ asset('assets') }}/";
+</script>
 
-            <!--end::Custom Javascript-->
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<!--begin::Global Javascript Bundle (mandatory)-->
+<script src="{{ asset('assets/plugins/global/plugins.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/scripts.bundle.js') }}"></script>
+<!--end::Global Javascript Bundle-->
 
+<!--begin::Vendors Javascript (used on specific pages)-->
+<script src="{{ asset('assets/plugins/custom/fullcalendar/fullcalendar.bundle.js') }}"></script>
+<script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/percent.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/radar.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/map.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/worldLow.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/continentsLow.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/usaLow.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZonesLow.js"></script>
+<script src="https://cdn.amcharts.com/lib/5/geodata/worldTimeZoneAreasLow.js"></script>
+<script src="{{ asset('assets/plugins/custom/datatables/datatables.bundle.js') }}"></script>
+<!--end::Vendors Javascript-->
 
-            @stack('scripts')
-            @stack('remarks')
-            @stack('json')
-            <x-livewiremodal-base />
-            @livewireScripts
-            <!--end::Javascript-->
+<!--begin::Custom Javascript-->
+<script src="{{ asset('assets/js/widgets.bundle.js') }}"></script>
+<script src="{{ asset('assets/js/custom/widgets.js') }}"></script>
+<script src="{{ asset('assets/js/custom/apps/chat/chat.js') }}"></script>
+<script src="{{ asset('assets/js/custom/utilities/modals/upgrade-plan.js') }}"></script>
+<script src="{{ asset('assets/js/custom/utilities/modals/create-campaign.js') }}"></script>
+<script src="{{ asset('assets/js/custom/utilities/modals/users-search.js') }}"></script>
+<script src="{{ asset('assets/js/custom/apps/customers/list/export.js') }}"></script>
+<script src="{{ asset('assets/js/custom/apps/customers/list/list.js') }}"></script>
+<!--end::Custom Javascript-->
+
+<!-- External libraries -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+
+@stack('scripts')
+@stack('remarks')
+@stack('json')
+
+<x-livewiremodal-base />
+@livewireScripts
+<!--end::Javascript-->
 </body>
 <!--end::Body-->
 
