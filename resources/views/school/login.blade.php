eea6aa84044407411e46d3de412e5b6e9970f64e
hanacesa <hanisa.hanim53@gmail.com>
2025-11-26 17:12:28 +0800

    pic

--- resources/views/school/login.blade.php
+++ resources/views/school/login.blade.php
@@ -53,8 +53,8 @@
 					<!--begin::Content-->
 					<div class="d-flex flex-column flex-center pb-0 pb-lg-10 p-10 w-100">
 						<!--begin::Image-->
-						<img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency.png" alt="" />
-						<img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/agency-dark.png" alt="" />
+						<img class="theme-light-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/membership.png" alt="" />
+						<img class="theme-dark-show mx-auto mw-100 w-150px w-lg-300px mb-10 mb-lg-20" src="assets/media/auth/membership-dark.png" alt="" />
 						<!--end::Image-->
 						<!--begin::Title-->
 						<h1 class="text-gray-800 fs-2qx fw-bold text-center mb-7">Fast, Efficient and Productive</h1>
@@ -147,12 +147,7 @@
 
 									
 
-									<!--begin::Sign up-->
-									<div class="text-gray-500 text-center fw-semibold fs-6 mt-4">
-										Not a Member yet?
-										<a href="/register" class="link-primary">Register Now!</a>
-									</div>
-									<!--end::Sign up-->
+									
 
                                     </form>
 								<!--end::Form-->
