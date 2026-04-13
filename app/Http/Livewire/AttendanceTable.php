5ecfe13aa28cfad41761dc8795a8d6d2b5677f3a
hanacesa <hanisa.hanim53@gmail.com>
2026-04-14 00:17:02 +0800

    updated attendance page for parent

--- app/Http/Livewire/AttendanceTable.php
+++ app/Http/Livewire/AttendanceTable.php
@@ -29,8 +29,8 @@ class AttendanceTable extends DataTableComponent
         $user = Auth::user();
 
         return Attendance::query()
-            ->whereHas('student', function ($query) use ($user) {
-                $query->where('ic', $user->ic);
+            ->whereHas('student.guardians', function ($query) use ($user) {
+                $query->where('ic', $user->ic); // match parent IC
             })
             ->when($this->columnSearch['student.name'] ?? null, function ($query, $studentName) {
                 return $query->whereHas('student', function ($subquery) use ($studentName) {
